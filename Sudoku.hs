15b68be10b0e36abc047a631fe8ed511d6613707
Karol Wieczorek <karol.wieczorek@outlook.com>
2015-01-16 22:16:44 +0100

    Added functions for getting number ine rows, columns and boxes

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,16 +1,11 @@
 module Sudoku (
-  loadBoardFromFile,
-  generateAllPossibilities,
-  solveBoard,
-  getNextMove,
-  applyMove
+  loadBoardFromFile
 ) where
 
-import Data.List
+import Data.List.Split
 
-type Row = [Int]
-type Board = [Row]
-type BoardPossibilities = [[[Int]]]
+type Field = Int
+type Board = [Field]
 
 allNums :: [Int]
 allNums = [1,2,3,4,5,6,7,8,9]
@@ -21,56 +16,21 @@ allIndexes = [0,1,2,3,4,5,6,7,8]
 -- public
 
 loadBoardFromFile :: String -> Board
-loadBoardFromFile content = map convertStringsToInts $ map words $ lines content
+loadBoardFromFile content = map convertStringToInt $ words content
 
-generateAllPossibilities :: Board -> BoardPossibilities
-generateAllPossibilities board = 
-  map (\ row -> map (generateAllPossibilitiesForField board row) allIndexes) allIndexes
-  where rows = numsInRows board
-        columns = numsInCols board
-        boxes = numsInBoxes board
-        removeAllExistingNums row col = ((allNums \\ rows !! row) \\ columns !! col) \\ boxes !! (row `div` 3) !! (col `div` 3)
-        generateAllPossibilitiesForField b row col = 
-          if b !! row !! col /= 0 then [b !! row !! col]
-          else removeAllExistingNums row col
+convertStringToInt :: String -> Int
+convertStringToInt str = if str == "_" then 0 else read str :: Int
 
-solveBoard :: Board -> Board
-solveBoard board =
-  if value == 0 then board
-  else solveBoard (applyMove board (row, col, value))
-  where (row, col, value) = getNextMove board (generateAllPossibilities board) 1 1
+numsForRows :: Board -> [[Int]]
+numsForRows board = chunksOf 9 board
 
-applyMove :: Board -> (Int, Int, Int) -> Board
-applyMove (row : anotherRows) (1, colNum, value) = (applyMoveToRow row (colNum, value) : anotherRows)
-  where applyMoveToRow (_ : anotherFields) (1, v) = (v : anotherFields)
-        applyMoveToRow (field : anotherFields) (c, v) = (field : applyMoveToRow anotherFields (c - 1, v))
-        applyMoveToRow [] (_, _) = []
-applyMove (row : anotherRows) (rowNum, colNum, value) = (row : applyMove anotherRows (rowNum - 1, colNum, value))
+numsForCols :: Board -> [[Int]]
+numsForCols board = map numsForCol allIndexes
+  where numsForCol col = [board !! (9 * i + col) | i <- allIndexes]
 
-getNextMove :: Board -> BoardPossibilities -> Int -> Int -> (Int, Int, Int)
-getNextMove [] _ _ _ = (0, 0, 0)
-getNextMove ([] : anotherRows) ([] : anotherPRows) row _ = getNextMove anotherRows anotherPRows (row + 1) 1
-getNextMove ((field : anotherFields) : anotherRows) ((possibilities : anotherPFields) : anotherPRows) row col =
-  if length possibilities == 1 && field == 0 then (row, col, head possibilities)
-  else getNextMove (anotherFields : anotherRows) (anotherPFields : anotherPRows) row (col + 1)
+numsForBoxes :: Board -> [[Int]]
+numsForBoxes board = map numsForBox allIndexes
+  where numsForBox n = [board !! (27 * (n `div` 3) + 3 * (n `mod` 3) + i) | i <- [0,1,2,9,10,11,18,19,20]]
 
--- private
-
-convertStringsToInts :: [String] -> [Int]
-convertStringsToInts stringsList = map (\x -> if x == "_" then 0 else read x) stringsList :: [Int]
-
-numsInRows :: Board -> [[Int]]
-numsInRows [] = []
-numsInRows (firstRow : anotherRows) = (delete 0 . nub) firstRow : numsInRows anotherRows
-
-numsInCols :: Board -> [[Int]]
-numsInCols ([] : _) = []
-numsInCols board = (delete 0 . nub . concatMap (\x -> [head x])) board : (numsInCols . map tail) board
-
-numsInBoxes :: Board -> [[[Int]]]
-numsInBoxes [] = []
-numsInBoxes board = (numsInBoxesAux . take 3) board : numsInBoxes (drop 3 board)
-
-numsInBoxesAux :: [[Int]] -> [[Int]]
-numsInBoxesAux [[], [], []] = []
-numsInBoxesAux rows = (filter (/= 0) . concatMap (take 3)) rows : (numsInBoxesAux . map (drop 3)) rows
+boxIndex :: Int -> Int
+boxIndex n = (n `div` 27) * 3 + (n `mod` 9 `div` 3)
--- main.hs
+++ main.hs
@@ -9,12 +9,4 @@ main = do
   else do
     content <- readFile $ args !! 0
     putStrLn "-----"
-    mapM_ print $ solveBoard $ loadBoardFromFile content
-    -- mapM_ print $ generateAllPossibilities $ loadBoardFromFile content
-    -- print $ getNextMove (loadBoardFromFile content) (generateAllPossibilities $ loadBoardFromFile content) 1 1
-    -- print (getMove content)
-    -- mapM_ print $ applyMove (board content) (getMove content)
-
-board content = loadBoardFromFile content
-poss content = generateAllPossibilities $ board content
-getMove content = getNextMove (board content) (poss content) 1 1
+    mapM_ print $ loadBoardFromFile content
